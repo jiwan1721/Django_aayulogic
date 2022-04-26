4b44969795bd4434879c832aa790d0fc747d129b
jiwan1721 <chandjiwn17@hotmail.com>
2022-04-26 08:05:54 +0545

    from home part3

--- poll/urls.py
+++ poll/urls.py
@@ -4,4 +4,7 @@ from .views import index
 
 urlpatterns =[
     path('',views.index,name='index'),
+    path('/poll/5',views.detail,name='detail'),
+    path('/poll/5/results',views.results,name='detail'),
+    path('/polls/5/vote',views.vote,name ='vote'),  
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,5 +1,28 @@
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from matplotlib.style import context
+from django.shortcuts import render
+from .models import question
 # Create your views here.
 def index(request):
-    return HttpResponse("hello wold, how are you")
+    latest_question_list = question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('poll/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    #output = ' ,'.join([q.question_text for q in latest_question_list])
+    return HttpResponse(template.render(context,request))
+def detail(request, questoin_id):
+    try:
+        question = question.objects.get(pk=question_id)
+    except question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'poll/detail.html',{'question':question})
+def results(request,question_id):
+    response = "you are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+def vote(request,question_id):
+    return HttpResponse(response % question_id)
