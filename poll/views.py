de74174f6433a2d2f855844087333a2f2a83276a
jiwan chanda <chandjiwn9@gmail.com>
2022-05-19 17:50:39 +0545

    testcase in django

--- poll/views.py
+++ poll/views.py
@@ -50,17 +50,19 @@ class index_view(generic.ListView):
     template_name = 'poll/index.html'
     context_object_name = 'latest_question_list'
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+            ).order_by('-pub_date')[:5]
+    
 class DetailView(generic.DetailView):
     model = Question
     template_name = "poll/detail.html"
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 class ResultView(generic.DetailView):
     model = Question
     template_name = "poll/results.html"
 
-def get_queryset(self):
-    return Question.objects.filter(
-        pub_date__lte = timezone.now()
-    ).order_by('-pub_date')[:5]
-    
+
     
